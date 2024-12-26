8515b972ae4d4995d9bc34404d9468a3f12d704d
jcerveto <joan.cerveto@ua.es>
2024-12-26 18:34:09 +0100

    feat: todos los saludos en una función

--- practicas/prac9/saludar.py
+++ practicas/prac9/saludar.py
@@ -2,15 +2,18 @@ def saludar_dca():
     print("Hola, soy DCA")
 
 
-def saludar_todos():
-    saludar_dca()
-
 def saludar_mads():
     print("Hola, soy MADS")
     
 def saludar_adi():
     print("Hola, soy ADI")
 
+
+
+def saludar_todos()
+    saludar_dca()
+    saludar_mads()
+    saludar_adi()
 
 if __name__ == "__main__":
     saludar_todos()
